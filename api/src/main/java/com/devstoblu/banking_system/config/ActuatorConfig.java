bfc978678c4b15065df1ef13bfe7fc2e9a915a82
jeanjacintho <jeanjacintho@yahoo.com.br>
2025-10-01 13:01:49 -0300

    fix: remoção do metrics direto no springboot

--- api/src/main/java/com/devstoblu/banking_system/config/ActuatorConfig.java
+++ api/src/main/java/com/devstoblu/banking_system/config/ActuatorConfig.java
@@ -1,6 +1,5 @@
 package com.devstoblu.banking_system.config;
 
-import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
 import io.micrometer.core.instrument.binder.system.UptimeMetrics;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +14,6 @@ public class ActuatorConfig {
     public MeterRegistryCustomizer<?> meterRegistryCustomizer() {
         return registry -> {
             try {
-                new ProcessorMetrics().bindTo(registry);
                 new UptimeMetrics().bindTo(registry);
             } catch (Exception e) {
                 System.err.println("Warning: Could not bind system metrics: " + e.getMessage());
