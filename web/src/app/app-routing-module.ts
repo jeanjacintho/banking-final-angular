52f6c4ea32f38aa92c8e449f31e948ab081aa154
jeanjacintho <jeanjacintho@yahoo.com.br>
2025-10-04 09:41:18 -0300

    fix: comentado a rota de login

--- web/src/app/app-routing-module.ts
+++ web/src/app/app-routing-module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Login } from './pages/login/login';
+//import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
 
 const routes: Routes = [
   { path: '', component: Home },
-  { path: 'login', component: Login }
+ // { path: 'login', component: Login }
 ];
 
 @NgModule({
