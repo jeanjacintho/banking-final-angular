88f43ceb7e059596bca80854cb599961d4d7a888
jeanjacintho <jeanjacintho@yahoo.com.br>
2025-10-06 11:10:38 -0300

    feat: tela de login

---
+++ web/src/app/pages/login/login.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+  loginForm: FormGroup;
+  isLoading = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      rememberMe: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.isLoading = true;
+      const { email, password, rememberMe } = this.loginForm.value;
+      
+      console.log('Login attempt:', { email, password, rememberMe });
+      
+      setTimeout(() => {
+        this.isLoading = false;
+        console.log('Login completed');
+      }, 2000);
+    } else {
+      this.markFormGroupTouched();
+    }
+  }
+
+  private markFormGroupTouched() {
+    Object.keys(this.loginForm.controls).forEach(key => {
+      const control = this.loginForm.get(key);
+      control?.markAsTouched();
+    });
+  }
+
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+}
