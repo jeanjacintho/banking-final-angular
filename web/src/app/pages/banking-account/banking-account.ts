d59b72d5c57a7371ce560a6c43a0f62bca02e3d2
mcalsing <marcelocalsing@gmail.com>
2025-10-14 19:48:10 -0300

    feat(investment):  alteracoes para usar docker no back e front

---
+++ web/src/app/pages/banking-account/banking-account.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banking-account',
+  standalone: false,
+  templateUrl: './banking-account.html',
+  styleUrl: './banking-account.css'
+})
+export class BankingAccount {
+
+}
